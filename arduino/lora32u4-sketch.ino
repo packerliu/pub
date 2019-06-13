6b20cf7d17bbd3b95846c9535a863e2c7d9ed178
Victor Liu <packerliu@gmail.com>
2019-06-13 07:22:42 -0700

    sketch added

---
+++ arduino/lora32u4-sketch.ino
@@ -0,0 +1,278 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example will send Temperature and Humidity
+ * using frequency and encryption settings matching those of
+ * the The Things Network. Application will 'sleep' 7x8 seconds (56 seconds)
+ *
+ * This uses OTAA (Over-the-air activation), where where a DevEUI and
+ * application key is configured, which are used in an over-the-air
+ * activation procedure where a DevAddr and session keys are
+ * assigned/generated for use with all further communication.
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate an AppEUI, DevEUI and AppKey.
+ * Multiple devices can use the same AppEUI, but each device has its own
+ * DevEUI and AppKey.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+/* VL
+ *  date: 2019-06-12
+ *  source: https://raw.githubusercontent.com/kersing/node-workshop/master/lora32u4-sketch/lora32u4-sketch.ino 
+ */
+ 
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include "LowPower.h"
+
+#include <Arduino.h>
+
+// DIO Pin mapping is hardware specific.
+// The unversioned board needs a wire bridge from pin DIO1 (at the end of the large connector) to D6 (on the smaller connector).
+// The v1.2 board needs the solder bridges connected, especially the DIO1 1 bridge.
+// Only docs for the unversioned board are available currently.
+// See: https://docs.bsfrance.fr/documentation/11355_LORA32U4II/Datasheet_LoRa32u4II_1.1.pdf
+//
+// Uncomment the define line below if you have a LoRa32u4II without a version number (v1.x)
+//#define LoRa32u4II_VERSION LoRa32u4II_1_1
+// Uncomment the define line below if you have a LoRa32u4II with version number (v1.2)
+#define LoRa32u4II_VERSION LoRa32u4II_1_2
+
+#define LoRa32u4II_1_1 11
+#define LoRa32u4II_1_2 12
+
+#if LoRa32u4II_VERSION == LoRa32u4II_1_1
+    #define DIO0 7
+    #define DIO1 6
+    #define DIO2 LMIC_UNUSED_PIN
+    #define RESET_PIN  1
+#elif LoRa32u4II_VERSION == LoRa32u4II_1_2
+    #define DIO0 0
+    #define DIO1 1
+    #define DIO2 2
+    #define RESET_PIN 4
+#else
+    #define DIO0 0
+    #define DIO1 0
+    #define DIO2 0
+    #define RESET_PIN 0
+    #error "Please specify which LoRa32u4 II version you have by commenting out the specific define line at the top of the ino file that starts with #define LoRa32u4II_VERSION"
+#endif
+
+int sleepcycles = 7;  // every sleepcycle will last 8 secs, total sleeptime will be sleepcycles * 8 sec
+bool joined = false;
+bool sleeping = false;
+#define LedPin 22     // pin 13 LED is not used, because it is connected to the SPI port
+
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+
+//VL: static const u1_t DEVEUI[8]  = { 0x76, 0xA0, 0xA6, 0x01, 0xAC, 0xC2, 0x8B, 0x00 };
+static const u1_t DEVEUI[8]  = { 0xB4, 0x4D, 0x72, 0xFB, 0x4C, 0x9B, 0x4F, 0x00 };
+
+//VL: static const u1_t APPEUI[8] = { 0xBD, 0x7C, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+static const u1_t APPEUI[8] = { 0x5C, 0xDB, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+// The key shown here is the semtech default key.
+//VL: static const u1_t APPKEY[16] = { 0xAE, 0x6A, 0xFA, 0xA3, 0x30, 0x9C, 0xEF, 0xE4, 0xAE, 0x69, 0x2F, 0x63, 0xC3, 0x34, 0x96, 0xF6 };
+static const u1_t APPKEY[16] = { 0xF6, 0x35, 0x6B, 0x4E, 0xD0, 0xE0, 0xE1, 0x4A, 0x83, 0x20, 0x8D, 0xD8, 0x24, 0x85, 0x84, 0xF0 };
+static void initfunc (osjob_t*);
+
+// provide APPEUI (8 bytes, LSBF)
+void os_getArtEui (u1_t* buf) {
+  memcpy(buf, APPEUI, 8);
+}
+
+// provide DEVEUI (8 bytes, LSBF)
+void os_getDevEui (u1_t* buf) {
+  memcpy(buf, DEVEUI, 8);
+}
+
+// provide APPKEY key (16 bytes)
+void os_getDevKey (u1_t* buf) {
+  memcpy(buf, APPKEY, 16);
+}
+
+static osjob_t sendjob;
+static osjob_t initjob;
+
+const lmic_pinmap lmic_pins = {
+    .nss = 8,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = RESET_PIN, // Needed on RFM92/RFM95? (probably not) D0/GPIO16 on v1.1
+    .dio = {DIO0, DIO1, DIO2}, // Specify pin numbers for DIO0, DIO1, DIO2
+};
+
+
+void onEvent (ev_t ev) {
+  int i,j;
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      // Disable link check validation (automatically enabled
+      // during join, but not supported by TTN at this time).
+      LMIC_setLinkCheckMode(0);
+      digitalWrite(LedPin,LOW);
+      // after Joining a job with the values will be sent.
+      joined = true;
+      break;
+    case EV_RFU1:
+      Serial.println(F("EV_RFU1"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      // Re-init
+      os_setCallback(&initjob, initfunc);
+      break;
+    case EV_TXCOMPLETE:
+      sleeping = true;
+        if (LMIC.dataLen) {
+        // data received in rx slot after tx
+        // if any data received, a LED will blink
+        // this number of times, with a maximum of 10
+        Serial.print(F("Data Received: "));
+        Serial.println(LMIC.frame[LMIC.dataBeg],HEX);
+        i=(LMIC.frame[LMIC.dataBeg]);
+        // i (0..255) can be used as data for any other application
+        // like controlling a relay, showing a display message etc.
+        if (i>10){
+          i=10;     // maximum number of BLINKs
+        }
+          for(j=0;j<i;j++)
+          {
+            digitalWrite(LedPin,HIGH);
+            delay(200);
+            digitalWrite(LedPin,LOW);
+            delay(400);
+          }
+      }
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      delay(50);  // delay to complete Serial Output before Sleeping
+
+      // Schedule next transmission
+      // next transmission will take place after next wake-up cycle in main loop
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    default:
+      Serial.println(F("Unknown event"));
+      break;
+  }
+}
+
+void do_send(osjob_t* j) {
+  byte buffer[22];
+  // It is a bad idea to send ASCII for real, only use this when developing!!
+  buffer[0]='H';
+  buffer[1]='i';
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, (uint8_t*) buffer, 2 , 0);
+    Serial.println(F("Sending: "));
+  }
+}
+
+// initial job
+static void initfunc (osjob_t* j) {
+    // reset MAC state
+    LMIC_reset();
+    LMIC_setClockError(MAX_CLOCK_ERROR * 1 / 100);
+    // start joining
+    LMIC_startJoining();
+    // init done - onEvent() callback will be invoked...
+}
+
+void setup()
+  {
+
+  delay(5000);
+  Serial.begin(9600);
+  Serial.println(F("Starting, waiting for 5 sec to init..."));
+  delay(5000);
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  os_setCallback(&initjob, initfunc);
+  LMIC_reset();
+}
+
+
+void loop()
+{
+    // start OTAA JOIN
+    if (joined==false)
+    {
+
+      os_runloop_once();
+
+    }
+    else
+    {
+      do_send(&sendjob);    // Sent sensor values
+      while(sleeping == false)
+      {
+        os_runloop_once();
+      }
+      sleeping = false;
+      for (int i=0;i<sleepcycles;i++)
+      {
+          LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);    //sleep 8 seconds
+      }
+    }
+
+      digitalWrite(LedPin,((millis()/100) % 2) && (joined==false)); // only blinking when joining and not sleeping
+
+}
--- arduino/readme.md
+++ arduino/readme.md
@@ -3,21 +3,30 @@
 Follow the instruction in [Workshop: Build a TTN LoRaWAN Node](https://github.com/kersing/node-workshop/blob/master/lora32u4.md):
 
 1. Install [Arduino IDE](https://www.arduino.cc/en/Main/Software)
-2. Adding "Adafruit AVR Boards" from "Tools -> Board Manager", then choose "Adafruit Feather 32U4" 
+2. Adding "Adafruit AVR Boards" from "Tools -> Board Manager", then choose "Adafruit Feather 32U4"
 3. Get the LMIC library, https://github.com/matthijskooijman/arduino-lmic
 4. Get the low power library, https://github.com/rocketscream/Low-Power
 5. Create a TTN account
 6. Create an Application under TTN Console, using following variables:
     - Application ID : ucite
     - Description : "Low Power Long Range Serial Link"
-Application EUI : leave empty, the value will be generated. 
+Application EUI : leave empty, the value will be generated.
 Handler registration : choose 'ttn-handler-us-west'.
 7. Click "Add Application" to create one.
 8. Receive App EUI as  "70B3D57ED001DB5C". Todo: hiding this.
 9. Adding a Node, using device name: "ard01".  Then we can see it at https://console.thethingsnetwork.org/applications/ucite/devices/ard01
 10. Creating the Arduino sketch, from sample at  https://raw.githubusercontent.com/kersing/node-workshop/master/lora32u4-sketch/lora32u4-sketch.ino
 11. To 'tune' this example to your TTN application you need to change:
-    - DEVEUI: 
-    - APPEUI: 
-    - APPKEY: 
-12. 
+    - DEVEUI:
+    - APPEUI:
+    - APPKEY:
+12. wiring difference between V1.2 and 1.1/1.0, https://www.thethingsnetwork.org/forum/t/big-lora32u4-boards-topic/15273
+
+# RPi3+Dragino Lora HAT
+
+1. LoRa - Raspberry Pi - Single Channel Gateway - Cheap!, https://www.hackster.io/ChrisSamuelson/lora-raspberry-pi-single-channel-gateway-cheap-d57d36
+2. Make it a systemctl service, https://medium.com/@benmorel/creating-a-linux-service-with-systemd-611b5c8b91d6
+3. Enable these services, https://www.linode.com/docs/quick-answers/linux/start-service-at-boot/
+3. Good article to read/review LoRa, https://medium.com/coinmonks/lpwan-lora-lorawan-and-the-internet-of-things-aed7d5975d5d
+4. enable GPS +RPi3 at https://wiki.dragino.com/index.php?title=Getting_GPS_to_work_on_Raspberry_Pi_3_Model_B
+5. Install Dual Channel forwarder, https://github.com/bokse001/dual_chan_pkt_fwd
